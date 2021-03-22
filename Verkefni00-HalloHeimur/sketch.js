0bc2db024fd445d8b6eaf6c019c1f5de17cf747a
sigridurbara <siggabm@gmail.com>
2021-03-22 15:57:17 +0000

    verkefni00-halloheimur

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -1,11 +1,36 @@
 // Hér kemur kóðinn þinn:
 
 function setup(){
-    createCanvas(300,300);
-    background(255,0,0);
-    fill(150);
+    createCanvas(600,400);
+    background(77,208,225);
+    fill(255);
     ellipse(100,100,50,50);
+    fill(255);
+    ellipse(200,100,50,50);
+    fill(255);
+    ellipse(300,100,50,50);
+    fill(255,204,188)
+    rect(200,100,50,50);
+    fill(255,204,188)
+    rect(300,100,50,50);
+    fill(255,204,188)
+    rect(400,100,50,50);
+    fill(255);
+    ellipse(400,200,50,50);
+    fill(255);
+    ellipse(200,200,50,50);
+    fill(255);
+    ellipse(300,200,50,50);
+    fill(200, 230, 201)
+    ellipse(200,230,65,65);
+    fill(200, 230, 201)
+    ellipse(300,230,65,65);
+    fill(200, 230, 201)
+    ellipse(100,230,65,65);
     fill(0);
     textSize(20);
     text("Halló heimur",200,40);
+    fill(0);
+    textSize(20);
+    text("Sigríður Bára",400,300);
 }
