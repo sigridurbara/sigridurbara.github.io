e56bbbaf9304a52826b65d5bd59dc6975833b338
sigridurbara <siggabm@gmail.com>
2021-03-22 15:30:30 +0000

    up

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -1,12 +1,11 @@
 // Hér kemur kóðinn þinn:
 
-function setup() {
-  createCanvas(400, 400);
-  background(255,200,0);
-  frameRate(4);
-}
-
-function draw() {
-  fill(random(0,255));
-  ellipse(200,200,50,50);
+function setup(){
+    createCanvas(300,300);
+    background(255,0,0);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(20);
+    text("Halló heimur",200,40);
 }
