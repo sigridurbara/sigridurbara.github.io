1e4e86426228e506e71ffc693b1784026e02f135
sigridurbara <siggabm@gmail.com>
2021-03-24 15:34:58 +0000

    verkefni01-mondrain

---
+++ Verkefni01-Mondrain/sketch.js
@@ -0,0 +1,39 @@
+// Hér kemur kóðinn þinn:
+
+function setup(){
+    createCanvas(225,225);
+    background(22,21,16);
+    noStroke()
+    //Rauður rect
+    fill(229,49,35);
+    rect(0,-1,95,120);
+    //Hvítur rect hægri efst
+    fill(236,237,232);
+    rect(100,-1,125,120);
+    //Hvítur rect vinstri neðst
+    fill(233,235,230);
+    rect(0,124,95,101);
+    //Hvítur rect í "miðju"
+    fill(237,239,228);
+    rect(100,124,105,87);
+    //Gulur rect
+    fill(252,201,43);
+    rect(100,216,59,12);
+    //Blár rect
+    fill(0,67,141);
+    rect(209,124,16,37);
+    //Hvítur rect vinstri neðst
+    fill(229,230,225);
+    rect(209,167,16,56);
+    //Svartur rect við hlið gula rect
+    fill(35,35,35);
+    rect(158,216,48,9);
+
+
+
+
+
+
+
+
+}
