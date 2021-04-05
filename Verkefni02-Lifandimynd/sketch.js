be7b933d079f32ca87a2617e1ac44d54bc5e7a1d
sigridurbara <siggabm@gmail.com>
2021-04-05 17:26:20 +0000

    up

--- Verkefni02-Lifandimynd/sketch.js
+++ Verkefni02-Lifandimynd/sketch.js
@@ -1,1 +1,12 @@
 // Hér kemur kóðinn þinn:
+
+function setup() {
+  createCanvas(800,500,);
+  background(0);
+    fill(69, 90, 100 );
+    noStroke();
+    triangle(250,445,400,180,545,445);
+    fill(0);
+    noStroke()
+    rect(348,159,105,100);
+}
