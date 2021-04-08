30f0e6519a59f2a76b79d7284fb899956cebcf32
sigridurbara <siggabm@gmail.com>
2021-04-08 11:07:05 +0000

    augu

--- Verkefni03-Fígúra/sketch.js
+++ Verkefni03-Fígúra/sketch.js
@@ -2,7 +2,8 @@
 
 var andlit = 115;
 var eyru = 60;
-var augu = 30;
+var minnieyru = 20 //inn í eyrun
+var augu = 40;
 
 function setup() {
   createCanvas(800,500);
@@ -11,17 +12,28 @@ function setup() {
 function draw() {
   background(178, 235, 242);
 // ANDLITIÐ
-  fill(84, 110, 122);
+  fill(120, 144, 156);
   rect(mouseX,mouseY,150,andlit,0,0,10,10);
 // EYRUN
   //vinstra eyra
-  fill(84, 110, 122);
-  triangle(mouseX-75,mouseY-andlit/2,mouseX-45,mouseY-andlit/2-eyru,mouseX-15,mouseY-andlit/2);
+  fill(120, 144, 156);
+  triangle(mouseX-75,mouseY-andlit/2,mouseX-45,mouseY-andlit/2-50,mouseX-15,mouseY-andlit/2);
   //hægra eyra
-  fill(84, 110, 122);
-  triangle(mouseX+15,mouseY-andlit/2,mouseX+45,mouseY-andlit/2-eyru,mouseX+75,mouseY-andlit/2);
+  fill(120, 144, 156);
+  triangle(mouseX+15,mouseY-andlit/2,mouseX+45,mouseY-andlit/2-50,mouseX+75,mouseY-andlit/2);
+  //inn í vinstra eyra
+  fill(13, 71, 161);
+  triangle(mouseX-55,mouseY-andlit/2,mouseX-45,mouseY-andlit/2-minnieyru,mouseX-35,mouseY-andlit/2);
+  //inn í hægra eyra
+  fill(13, 71, 161);
+  triangle(mouseX+35,mouseY-andlit/2,mouseX+45,mouseY-andlit/2-minnieyru,mouseX+55,mouseY-andlit/2);
 //AUGUN
   //vinstra auga
+  fill(0, 162, 232);
+  ellipse(mouseX-augu/2-17.5,mouseY-augu/2,augu,augu);
+  //hægra auga
+  fill(0, 162, 232);
+  ellipse(mouseX+augu/2+17.5,mouseY-augu/2,augu,augu);
 
   //ellipse(mouseX,mouseY - bukur/2, 80,80);
   // AUGUN
