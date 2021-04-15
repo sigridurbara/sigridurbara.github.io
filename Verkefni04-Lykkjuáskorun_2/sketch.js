18e1c24f59494326e65393b83e797651f470ca1e
sigridurbara <siggabm@gmail.com>
2021-04-15 12:01:50 +0000

    up

--- Verkefni04-Lykkjuáskorun_2/sketch.js
+++ Verkefni04-Lykkjuáskorun_2/sketch.js
@@ -4,15 +4,15 @@ function setup() {
   createCanvas(400,400);
   background(255, 215, 0);
   strokeWeight(2);
-  frameRate(3);
+  frameRate(2);
   colorMode(HSB);
 }
-
+//Default mode er ellipseMode(CENTER)
 function draw() {
-  for(var x = 10; x < width; x = x+20) {
+  for(var x = 300; x < width; x = x-15) {
     fill(random(180,350),70,100);
-    for(var y = 10; y < height; y = y+20) { 
-        ellipse(x, y, 15, 15);
+    for(var y = 300; y < height; y = y-15) {
+        ellipse(200, 200, x, y);
     }
   }
 }
