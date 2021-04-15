d01661e3a1c453168536328c35573117edb6e2fa
sigridurbara <siggabm@gmail.com>
2021-04-15 11:28:07 +0000

    verkefni04 - askorun1

--- Verkefni04-Lykkjuáskorun/sketch.js
+++ Verkefni04-Lykkjuáskorun/sketch.js
@@ -1,1 +1,18 @@
 // Hér kemur kóðinn þinn:
+
+function setup() {
+  createCanvas(400,400);
+  background(255, 215, 0);
+  strokeWeight(2)
+  frameRate(3);
+  colorMode(HSB)
+}
+
+function draw() {
+  for(var x = 10; x < width; x = x+20){
+    fill(random(180,350),70,100);
+    for(var y = 10; y < height; y = y+20){
+        ellipse(x, y, 15, 15);
+    }
+  }
+}
