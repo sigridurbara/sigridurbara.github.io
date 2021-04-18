2f5d20d1ffe6a010fa6d105a5ec52b95bf526805
sigridurbara <siggabm@gmail.com>
2021-04-18 17:19:46 +0000

    up

--- Verkefni04-Lykkjuáskorun_3/sketch.js
+++ Verkefni04-Lykkjuáskorun_3/sketch.js
@@ -9,10 +9,10 @@ function setup() {
 function draw() {
   background(255, 215, 0);
   for(var y = 20; y < height; y = y+30) {
+    fjöldi = random(13);
+    for(var k = 0; k < fjöldi; k++){
+      ellipse(k*30,y,10,10);
 
-    //fill(random(180,350),70,100);
-    //for(var y = 10; y < height; y = y+20) {
-        //ellipse(x, y, 15, 15);
+      }
     }
   }
-}
