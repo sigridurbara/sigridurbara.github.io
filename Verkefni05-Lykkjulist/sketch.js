e33d4093ced93942e137e892d5d8f9b708bb1670
sigridurbara <siggabm@gmail.com>
2021-04-19 21:33:55 +0000

    up

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -13,13 +13,14 @@ function setup() {
 
 function draw() {
   push();
-  for(var x = 0; x < width; x = x+30) {
-    fill(random(180,350),70,100);
-    for(var y = 10; y < height; y = y+20) {
+  for(var x = 30; x < width; x = x+30) {
+    fill(random(100,160),50,80);
+      for(var y = 30; y < height; y = y+30) {
       ellipse(x, y, 15, 15);
+      }
     }
   }
-  }
+
   pop();
 
   push();
