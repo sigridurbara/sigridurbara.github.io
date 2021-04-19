3d7bf050570b3f0bc0eeda6b83d70259401b18fc
sigridurbara <siggabm@gmail.com>
2021-04-19 21:55:36 +0000

    verkefni 06

---
+++ Verkefni06-Skjáskipting/sketch.js
@@ -0,0 +1,51 @@
+// Hér kemur kóðinn þinn:
+
+//fjöldiK = fjöldi krónublaða
+var fjöldiK = 15
+
+function setup() {
+  createCanvas(450,600);
+  colorMode(HSB);
+  background(random(160,360),50,100);
+  angleMode(DEGREES);
+  frameRate(2);
+  }
+
+function draw() {
+  bakgrunnur();
+  blóm();
+function bakgrunnur(){
+  push();
+  for(var x = 20; x < width-20; x = x+20) {
+    fill(random(180,350),65,100);
+      for(var y = 20; y < height-20; y = y+20) {
+      ellipse(x, y, 15, 15);
+
+    }
+  }
+  pop();
+}
+
+function blóm(){
+  push();
+  translate(width/2, height/3);
+//stilkurinn
+  fill(180,80,100);
+  noStroke();
+  stroke(0,0,0);
+  rect(-2,0,4,140);
+//miðjan í blóminu
+  fill(180,80,100);
+  ellipse(0, 0,20,20);
+//krónublöðin
+  for (var i = 0; i<fjöldiK; i++){
+    angle = 24
+    rotate(angle);
+    fill(0,0,100);
+    strokeWeight(1);
+    stroke(0,0,0);
+    ellipse(35,0,50,10);
+  }
+  pop();
+  }
+}
