5ea5788ea2d457015e040c1e9a59f462cf7d992a
sigridurbara <siggabm@gmail.com>
2021-04-19 15:54:28 +0000

    up

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -1,11 +1,12 @@
 // Hér kemur kóðinn þinn:
 
-
+var fjöldiK = 12
 
 function setup() {
   createCanvas(450,600);
   colorMode(HSB);
   background(random(160,360),50,100);
+  angleMode(DEGREES);
   //strokeWeight(2);
   //frameRate(3);
   }
@@ -13,7 +14,15 @@ function setup() {
 function draw() {
   translate(width/2, height/3);
   fill(180,80,100);
-  
+  ellipse(0, 0,20,20);
+  for (var i = 0; i<fjöldiK; i++){
+    angle = 60
+    rotate(angle);
+    fill(0,0,100);
+    ellipse(50,0,50,20);
+  }
 
 
-//}
+
+
+}
