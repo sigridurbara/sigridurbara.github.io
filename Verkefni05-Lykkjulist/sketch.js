1b00a1966b6365cd8260778975bebadb5f08da52
sigridurbara <siggabm@gmail.com>
2021-04-19 15:44:47 +0000

    up

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -1,15 +1,19 @@
 // Hér kemur kóðinn þinn:
 
+
+
 function setup() {
   createCanvas(450,600);
   colorMode(HSB);
-  background(random(360),50,100);
+  background(random(160,360),50,100);
   //strokeWeight(2);
   //frameRate(3);
-
-}
+  }
 
 function draw() {
+  translate(width/2, height/3);
+  fill(180,80,100);
+  
 
 
 //}
