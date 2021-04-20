72d6ce60eaeefd7c5c7c3c34262ea5ecdd2c4e40
sigridurbara <siggabm@gmail.com>
2021-04-20 09:03:29 +0000

    up

--- Verkefni06-Skjáskipting/sketch.js
+++ Verkefni06-Skjáskipting/sketch.js
@@ -1,54 +1,31 @@
 // Hér kemur kóðinn þinn:
 
 //fjöldiK = fjöldi krónublaða
-var y = 10x + height/3
 
 function setup() {
-  createCanvas(450,600);
-  //colorMode(HSB);
-  background(0,0,100);
+  createCanvas(500,500);
+  colorMode(HSB);
   //angleMode(DEGREES);
   //frameRate(2);
   }
 
 function draw() {
   bakgrunnur();
-  blóm();
+  //blóm();
 
 function bakgrunnur(){
   push();
-  translate(width/3,height/3);
+  background(0,0,100);
+  translate(width/2,height/2);
   if(mouseX>width/3 && mouseY>height/2);
-    for(var x = 20; x < width-20; x = x+20) {
-    fill(random(180,350),65,100);
-      for(var y = 20; y < height-20; y = y+20) {
-      ellipse(x, y, 15, 15);
+    for(var x = width/2; x < height+200; x = x+50) {
+      fill(random(180,350),65,100);
+      for(var y = width/2+50; y < height+200; y = y+50){
+      angle = 50
+      rotate(angle);
+      triangle(0,0,);
+      }
 
     }
-  }
-  pop();
-}
-
-function blóm(){
-  push();
-  translate(width/2, height/3);
-//stilkurinn
-  fill(180,80,100);
-  noStroke();
-  stroke(0,0,0);
-  rect(-2,0,4,140);
-//miðjan í blóminu
-  fill(180,80,100);
-  ellipse(0, 0,20,20);
-//krónublöðin
-  for (var i = 0; i<fjöldiK; i++){
-    angle = 24
-    rotate(angle);
-    fill(0,0,100);
-    strokeWeight(1);
-    stroke(0,0,0);
-    ellipse(35,0,50,10);
-  }
-  pop();
   }
 }
