61db902c1681838764ee368af85aab28b0d3acb4
sigridurbara <siggabm@gmail.com>
2021-04-20 11:03:56 +0000

    up

--- Verkefni06-Skjáskipting/sketch.js
+++ Verkefni06-Skjáskipting/sketch.js
@@ -10,20 +10,38 @@ function setup() {
   }
 
 function draw() {
-  bakgrunnur();
-  //blóm();
-
-function bakgrunnur(){
-  push();
   background(0);
-  //translate(width/2,height/2);
+  grænnFerningur();
+
+function grænnFerningur(){
+  push();
+  //background(0);
   fill(34, 139, 34);
   if(mouseX>width/2 && mouseY<height/3){
     rect(width/2,-5,width/2,height/3+5);
   }
-
-
-
-    //}
+  else{
+    fill(143, 188, 139);
+    rect(width/2,-5,width/2,height/3+5);
   }
+  pop();
+}
+
+function fjólublárFerningur(){
+  push();
+  fill(139, 0, 139);
+  if(mouseX<width/2 && mouseY>height-100){
+    rect();
+  }
+  else{
+    fill(106, 90, 205);
+    rect();
+  }
+}
+
+
+
+
+
+  //}
 }
