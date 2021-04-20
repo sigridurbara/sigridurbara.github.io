fc1afb0356a4a20eb5dfb02cbab5448c7cf16a31
sigridurbara <siggabm@gmail.com>
2021-04-20 09:33:04 +0000

    up

--- Verkefni06-Skjáskipting/sketch.js
+++ Verkefni06-Skjáskipting/sketch.js
@@ -1,12 +1,12 @@
 // Hér kemur kóðinn þinn:
 
-//fjöldiK = fjöldi krónublaða
+
 
 function setup() {
   createCanvas(500,500);
-  colorMode(HSB);
+  colorMode(RGB);
   //angleMode(DEGREES);
-  //frameRate(2);
+  frameRate(2);
   }
 
 function draw() {
@@ -15,16 +15,14 @@ function draw() {
 
 function bakgrunnur(){
   push();
-  background(0,0,100);
+  background(255);
   translate(width/2,height/2);
-  if(mouseX>width/3 && mouseY>height/2);
-    for(var x = width/2; x < height+200; x = x+50) {
-      fill(random(180,350),65,100);
-      for(var y = width/2+50; y < height+200; y = y+50){
-      angle = 50
-      rotate(angle);
-      triangle(0,0,);
-      }
+  fill(34, 139, 34);
+  if(mouseX>width/2 && mouseY<height/3){
+    
+  }
+
+
 
     }
   }
