879e316ac1989130e8eb28d170b0a8cd001c7f03
sigridurbara <siggabm@gmail.com>
2021-04-20 12:09:48 +0000

    up

--- Verkefni06-Skjáskipting/sketch.js
+++ Verkefni06-Skjáskipting/sketch.js
@@ -1,23 +1,28 @@
 // Hér kemur kóðinn þinn:
 
-
+//translate x-hnit : fjólubláBlóm
+var x = random(width/2+20,width/2-20)
+//translate y-hnit : fjólubláBlóm
+var y = random(height/3+20,height/3*2-20)
 
 function setup() {
   createCanvas(500,500);
   colorMode(RGB);
-  //angleMode(DEGREES);
+  noStroke();
   frameRate(2);
   }
 
 function draw() {
   background(255);
   grænnFerningur();
+  fjólubleikurFerningur();
   fjólublárFerningur();
 
+
 function grænnFerningur(){
-  //push();
+  push();
   //background(0);
-  fill(34, 139, 34);
+  fill(34, 139, 34,95);
   if(mouseX>width/2 && mouseY<height/3){
     rect(width/2,-5,width/2,height/3+5);
   }
@@ -25,25 +30,38 @@ function grænnFerningur(){
     fill(143, 188, 139);
     rect(width/2,-5,width/2,height/3+5);
   }
-  //pop();
+  pop();
+}
+
+function fjólubleikurFerningur(){
+  push();
+  fill(106, 90, 205);
+  if(mouseX<width/2 && mouseY>height-50){
+    rect(0,height-50,width/2,height-50);
+  }
+  else{
+    fill(139, 0, 139);
+    rect(0,height-50,width/2,height-50);
+  }
+  pop();
 }
 
 function fjólublárFerningur(){
-  //push();
-  fill(139, 0, 139);
-  if(mouseX<width/2 && mouseY>height-100){
-    rect(0,height,width/2,height-100);
-  }
-  else{
-    fill(106, 90, 205);
-    rect(0,height,width/2,height-100);
-  }
-  //pop();
+  push();
+  fill(147, 112, 219,80);
+  rect(width/2,height/3,width/2,height/3*2);
+  pop();
 }
 
+function fjólubláBlóm(){
+  push();
+  translate(x,y);
+  noStroke();
+  for (let i = 0; i < 10; i ++) {
+    ellipse(x, y, 20, 80);
+    rotate(PI/5);
+  }
+  pop();
+}
 
-
-
-
-  //}
 }
