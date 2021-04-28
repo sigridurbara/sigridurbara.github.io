d6fbf326276ee227034c1a30377579271bd06ece
sigridurbara <siggabm@gmail.com>
2021-04-28 14:50:55 +0000

    up

--- Verkefni07-Skopp/sketch.js
+++ Verkefni07-Skopp/sketch.js
@@ -1,7 +1,7 @@
 // Hér kemur kóðinn þinn:
 
-var boltiX = 100;
-var boltiY = 100;
+var boltiX = 300;
+var boltiY = 300;
 var hradiX = 5;
 var hradiY = -6;
 var boltiStaerd = 20;
@@ -12,7 +12,7 @@ var spadiY;
 
 function setup() {
 	createCanvas(400,500);
-	spadiY = height - 45;
+	spadiY = height - 455;
 	rectMode(CENTER);
 	textFont("Courier New",16);
 	textStyle(BOLD);
@@ -32,17 +32,18 @@ function draw() {
 		hradiX = hradiX * -1;
 	}
   // Athuga hvort boltinn snertir þakið
-  if ((boltiY < boltiStaerd)) {
+  if ((boltiY+boltiStaerd/2 > height)){ //< boltiStaerd)) {
     hradiY = hradiY * -1;
   }
   //Athuga hvort boltinn snertir spaðann
 	if (boltiX-boltiStaerd/2 > mouseX - spadiBreidd/2 &&
     boltiX+boltiStaerd < mouseX + spadiBreidd/2 &&
-		boltiY+boltiStaerd > spadiY + spadiThykkt/4) {
+		boltiY+boltiStaerd < spadiY - spadiThykkt/4) { //LAGA!!!!!!!!!
     hradiY = hradiY * -1;
 		stig = stig+1;
 
   }
+	//Ef boltinn fer út af
 
 
 	// Teikna boltann
@@ -50,9 +51,9 @@ function draw() {
   rect(boltiX, boltiY, boltiStaerd, boltiStaerd);
 	// Teikna spaðann
   fill(46, 204, 113); //upphaflegi liturinn: fill(3,123,227);
-  rect(mouseX,spadiY ,spadiBreidd,spadiThykkt);
+  rect(mouseX,spadiY,spadiBreidd,spadiThykkt);
   fill (0);
-  ellipse(mouseX, spadiY, 5,5);
+  ellipse(mouseX, spadiY, 3,3);
 	// Teikna stigin
   fill(0);
   text("Stig: " + stig,10,20);
