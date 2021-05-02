9259336fcbd83945cc5aa86b0d419df2114c908e
sigridurbara <siggabm@gmail.com>
2021-05-02 16:41:24 +0000

    up

--- Verkefni08-Fígúruskopp/sketch.js
+++ Verkefni08-Fígúruskopp/sketch.js
@@ -43,6 +43,7 @@ function draw() {
 				hradiY = hradiY * -1;
 				hradiX = hradiX * -1;
 	 	}
+}
 
 function kisa(x,y,andlit,minnieyru,augu){
 	rectMode(CENTER);
@@ -109,47 +110,3 @@ function kisa(x,y,andlit,minnieyru,augu){
 	if(stig>14){
 		background(52,152,219);
 	}
-
-
-	//Ef boltinn fer út af
-
-	//teikna boltann
-		push();
-			noStroke();
-			//angleMode(DEGREES);
-			//angle = 45
-	  //miðjan á boltanum
-	    fill(23, 165, 137);
-	    rect(boltiX, boltiY, boltiStaerd, boltiStaerd) //0, 0,7,7);
-	  //armarnir
-			//armar hægra megin við miðju
-	      fill(255);
-	      ellipse(boltiX+20,boltiY-5,10,5);
-	      ellipse(boltiX+20,boltiY+5,10,5);
-			//armar vinstra megin við miðju
-				ellipse(boltiX-20,boltiY+5,-10,5);
-				ellipse(boltiX-20,boltiY-5,-10,5);
-			//armar fyrir ofan miðju
-				ellipse(boltiX+5,boltiY-20,5,10);
-				ellipse(boltiX-5,boltiY-20,5,10);
-			//armar fyrir neðan miðju
-				ellipse(boltiX+5,boltiY+20,5,10);
-				ellipse(boltiX-5,boltiY+20,5,10);
-			pop();
-
-
-	// Teikna boltann
-  //fill(236,36,94);
-  //rect(boltiX, boltiY, boltiStaerd, boltiStaerd);
-
-	// Teikna spaðann
-  fill(46, 204, 113); //upphaflegi liturinn: fill(3,123,227);
-  rect(mouseX,spadiY,spadiBreidd,spadiThykkt);
-  fill (0);
-  ellipse(mouseX, spadiY, 3,3);
-	// Teikna stigin
-  fill(0);
-  text("Stig: " + stig,10,480);
-
-
-}
