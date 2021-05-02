d6b1880bc897aaea287d4c8c5cf5a387a738bb5e
sigridurbara <siggabm@gmail.com>
2021-05-02 16:40:10 +0000

    mousex og mousey breytt i x og y

--- Verkefni08-Fígúruskopp/sketch.js
+++ Verkefni08-Fígúruskopp/sketch.js
@@ -49,45 +49,45 @@ function kisa(x,y,andlit,minnieyru,augu){
 	strokeWeight(2);
 // ANDLITIÐ
   fill(120, 144, 156);
-  rect(mouseX,mouseY,150,andlit,0,0,10,10);
+  rect(x,y,150,andlit,0,0,10,10);
 // EYRUN
   //vinstra eyra
   fill(120, 144, 156);
-  triangle(mouseX-75,mouseY-andlit/2,mouseX-45,mouseY-andlit/2-50,mouseX-15,mouseY-andlit/2);
+  triangle(x-75,y-andlit/2,x-45,y-andlit/2-50,x-15,y-andlit/2);
   //hægra eyra
   fill(120, 144, 156);
-  triangle(mouseX+15,mouseY-andlit/2,mouseX+45,mouseY-andlit/2-50,mouseX+75,mouseY-andlit/2);
+  triangle(x+15,y-andlit/2,x+45,y-andlit/2-50,x+75,y-andlit/2);
   //inn í vinstra eyra
   fill(13, 71, 161);
-  triangle(mouseX-55,mouseY-andlit/2,mouseX-45,mouseY-andlit/2-minnieyru,mouseX-35,mouseY-andlit/2);
+  triangle(x-55,y-andlit/2,x-45,y-andlit/2-minnieyru,x-35,y-andlit/2);
   //inn í hægra eyra
   fill(13, 71, 161);
-  triangle(mouseX+35,mouseY-andlit/2,mouseX+45,mouseY-andlit/2-minnieyru,mouseX+55,mouseY-andlit/2);
+  triangle(x+35,y-andlit/2,x+45,y-andlit/2-minnieyru,x+55,y-andlit/2);
 //AUGUN
   //vinstra auga
   fill(0, 162, 232);
-  ellipse(mouseX-augu/2-14.5,mouseY-augu/2,augu,augu);
+  ellipse(x-augu/2-14.5,y-augu/2,augu,augu);
   //hægra auga
   fill(0, 162, 232);
-  ellipse(mouseX+augu/2+14.5,mouseY-augu/2,augu,augu);
+  ellipse(x+augu/2+14.5,y-augu/2,augu,augu);
   //vinstri augasteinn
   fill(0);
-  ellipse(mouseX-augu/2-14.5,mouseY-augu/2,augu/5,augu/1.5);
+  ellipse(x-augu/2-14.5,y-augu/2,augu/5,augu/1.5);
   //hægri augasteinn
   fill(0);
-  ellipse(mouseX+augu/2+14.5,mouseY-augu/2,augu/5,augu/1.5);
+  ellipse(x+augu/2+14.5,y-augu/2,augu/5,augu/1.5);
 //NEFIÐ
   fill(0);
-  triangle(mouseX-4,mouseY+8,mouseX,mouseY+16,mouseX+4,mouseY+8);
+  triangle(x-4,y+8,x,y+16,x+4,y+8);
 //VEIÐIHÁRIN
   //hægri veiðihár
-  line(mouseX-augu-augu,mouseY,mouseX-35,mouseY+12); //efsta veiðihár
-  line(mouseX-augu-augu,mouseY+20,mouseX-38,mouseY+20); //miðju veiðihá
-  line(mouseX-augu-augu,mouseY+40,mouseX-35,mouseY+28); //neðsta veiðihár
+  line(x-augu-augu,y,x-35,y+12); //efsta veiðihár
+  line(x-augu-augu,y+20,x-38,y+20); //miðju veiðihá
+  line(x-augu-augu,y+40,y-35,y+28); //neðsta veiðihár
   //vinstri veiðihár
-  line(mouseX+35,mouseY+12,mouseX+augu+augu,mouseY); //efsta veiðihár
-  line(mouseX+38,mouseY+20,mouseX+augu+augu,mouseY+20); //miðju veiðihár
-  line(mouseX+35,mouseY+28,mouseX+augu+augu,mouseY+40); //neðsta veiðihár
+  line(x+35,y+12,x+augu+augu,y); //efsta veiðihár
+  line(x+38,y+20,x+augu+augu,y+20); //miðju veiðihár
+  line(x+35,y+28,x+augu+augu,y+40); //neðsta veiðihár
 
 }
 
